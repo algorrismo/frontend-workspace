3b681f3f897377284fb31779b35fe3575ab112f7
Ismail Hossain <heythereitsfahim@gmail.com>
2026-09-22 09:09:55 +0600

    used state fucntion

--- week-01/src/components/state.jsx
+++ week-01/src/components/state.jsx
@@ -4,35 +4,35 @@ const State = () => {
 
     const [count, setCount]= useState(0)
 
-    function incrementCount(){
-        setCount(count+1)
-    }
+    // function incrementCount(){
+    //     setCount(count+1)
+    // }
 
-    // to test the current user email 
-    function decrementCount(){
+    // function decrementCount(){
 
-        if(count > 0)
-            setCount(count-1)
-    }
+    //     if(count > 0)
+    //         setCount(count-1)
+    // }
+
+
     return (
 
         <div>
             <div>
                 <p className='text-center text-cyan-500 text-3xl font-bold'>This is a state component</p>
             </div>
+                {/* display the current count */}
                 <p className='text-center text-white text-2xl font-bold'>
                     {count}
                 </p>
             <div>
                 {/* this is where state will be managed*/}
                 <div className='flex justify-center gap-5 mt-5'>
-                    <button onClick={incrementCount} className='bg-green-800 p-2 w-12'>+1
+                    <button onClick={()=> setCount(count+1)} className='bg-green-800 p-2 w-12'>+1
                     </button>
-                    <button onClick={decrementCount} className='bg-red-800 p-2 w-12'>-1
+                    <button onClick={()=> setCount(count-1)} className='bg-red-800 p-2 w-12'>-1
                     </button>
                 </div>
-
-                {/* display the current count */}
             </div>
         </div>
     );

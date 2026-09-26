14419164d452e23a3df696372e1caea1a2923ed8
Ismail Hossain <heythereitsfahim@gmail.com>
2026-09-26 21:15:52 +0600

    render blog post by slug in centered post card

--- my-app/src/app/blog/[slug]/page.jsx
+++ my-app/src/app/blog/[slug]/page.jsx
@@ -1,8 +1,39 @@
+import blogPosts from '@/blogPostData/data';
 import React from 'react';
 
-const page = () => {
+// const page = async (props) => {
+//     const params = props.params;
+// }
+
+const page = async({ params }) => {
+    
+    //? const slug =  params.slug
+    //* const data = await params;
+    //* const slug = data.slug;
+
+    //? mainly a cleaner/shorter version of the example above:
+    const { slug } = await params;
+    
+    const data =await params;
+    const slug2 = data.slug;
+    
+    console.log("slug:", slug);
+    console.log("slug2:", slug2);
+
+    //? find the post with the matching slug
+    const post = blogPosts.find((post)=> post.slug === slug)
+
+    // console.log("post:", post);
+
+    if (!post) {
+        return <div>Post not found</div>;
+    }
+
     return (
-        <div>
+        <div className='flex justify-between flex-col mt-30 mx-auto space-y-2 border-2 border-orange-700 w-180 rounded-2xl p-5 text-center'>
+
+            <h1 className='text-2xl font-bold'>{post.title}</h1>
+            <p className='text-gray-600'>{post.content}</p>
             
         </div>
     );
